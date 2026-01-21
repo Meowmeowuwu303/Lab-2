40543fc28a730e32be4443058b386e8073c2b139
Володимир Крісанов <volodimirkrisanov@gmail.com>
2026-01-21 13:38:42 +0100

    Modify properties and references of objects

--- Lab2.4.js
+++ Lab2.4.js
@@ -1,1 +1,19 @@
+'use strict';
 
+// Об'єкти
+const constantObj = { name: 'Initial value' };
+let variableObj = { name: 'Another value' };
+
+console.log('Before modification:');
+console.dir({ constant: constantObj.name, variable: variableObj.name });
+
+constantObj.name = 'Modified constant';
+variableObj.name = 'Modified variable';
+
+console.log('After property modification:');
+console.dir({ constant: constantObj.name, variable: variableObj.name });
+
+variableObj = { name: constantObj.name };
+
+console.log('After reference change:');
+console.dir({ constant: constantObj.name, variable: variableObj.name });
