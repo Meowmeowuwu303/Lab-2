07910cc1837323995bf2b5ee56766e08ff23a8bd
Володимир Крісанов <volodimirkrisanov@gmail.com>
2026-01-21 13:38:24 +0100

    Add functions for average, square, and cube calculations

--- Lab2.3.js
+++ Lab2.3.js
@@ -1,1 +1,22 @@
+'use strict';
 
+// Функції
+const average = (a, b) => (a + b) / 2;
+
+const square = (x) => x ** 2;
+
+const cube = (x) => x ** 3;
+
+const calculate = () => {
+  const results = [];
+  for (let i = 0; i <= 9; i++) {
+    const avg = average(square(i), cube(i));
+    results.push(avg);
+  }
+  return results;
+};
+
+console.log('Average of 10 and 25:', average(10, 25));
+console.log('Square of 7:', square(7));
+console.log('Cube of 4:', cube(4));
+console.log('Calculations:', calculate());
