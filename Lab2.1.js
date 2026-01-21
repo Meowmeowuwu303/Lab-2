59fba8b5c7f467638876c3a8d7d6b6af769a54b0
Володимир Крісанов <volodimirkrisanov@gmail.com>
2026-01-21 13:37:45 +0100

    Add greeting function for user's birthday

--- Lab2.1.js
+++ Lab2.1.js
@@ -1,1 +1,12 @@
+'use strict';
 
+// Ідентифікатори
+const userName = 'Oleksiy';
+const birthYear = 2005;
+
+const createGreeting = (name) => {
+  const age = 2025 - birthYear;
+  return `Congratulations on your ${age}th birthday, ${name}!`;
+};
+
+console.log(createGreeting(userName));
