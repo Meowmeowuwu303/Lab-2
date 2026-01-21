1b26200fae84910f7cfd63c0eec21e39b874f64d
Володимир Крісанов <volodimirkrisanov@gmail.com>
2026-01-21 13:38:02 +0100

    Add functions to generate number ranges

--- Lab2.2.js
+++ Lab2.2.js
@@ -1,1 +1,21 @@
+'use strict';
 
+// Цикли
+const range = (start, end) => {
+  const arr = [];
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const rangeOdd = (start, end) => {
+  const arr = [];
+  for (let i = start; i <= end; i += 2) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+console.log('All numbers:', range(20, 35));
+console.log('Odd numbers:', rangeOdd(20, 35));
